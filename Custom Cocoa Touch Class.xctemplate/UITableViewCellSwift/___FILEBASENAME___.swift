809d465bfb34c42a9fa6dcb58badf881f34c4351
BANYAN <jesusblessher@gmail.com>
2019-07-16 18:43:40 +0800

    update Swift UITableViewCell

--- Custom Cocoa Touch Class.xctemplate/UITableViewCellSwift/___FILEBASENAME___.swift
+++ Custom Cocoa Touch Class.xctemplate/UITableViewCellSwift/___FILEBASENAME___.swift
@@ -8,12 +8,12 @@ class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
     
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
     
